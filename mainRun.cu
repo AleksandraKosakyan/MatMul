2a3a46e8ee4cf4051f56ee40cffc953b6d3248ff
AleksandraKosakyan <67643023+AleksandraKosakyan@users.noreply.github.com>
2020-07-06 00:11:09 +0400

    Update mainRun.cu

--- mainRun.cu
+++ mainRun.cu
@@ -16,24 +16,21 @@ printf("Cuda error: %s\n", cudaGetErrorString(err));    \
 printf("Error in file: %s, line: %i\n", __FILE__, __LINE__);  \
 }                 \
 
-/**
-* Метод уножения матриц на GPU
-* @params: matrix A, matrix B, result matrix C
-*/
+
 
 __global__ void matMult(float* A, float* B, float* C){
-	// Индекс блока
+
 	int bx = blockIdx.x;
 	int by = blockIdx.y;
 
-	// Индекс нити
+
 	int tx = threadIdx.x;
 	int ty = threadIdx.y;
 
 	float sum = 0.0;
-	//Индекс A[i][0]
+
 	int ia = N * BLOCK_SIZE * by + N * ty;
-	// Индекс B[0][j]
+
 	int ib = BLOCK_SIZE * bx + tx;
 
 
@@ -43,15 +40,12 @@ __global__ void matMult(float* A, float* B, float* C){
 	// Индекс C[i][j]
 	int ic = N * BLOCK_SIZE * by + BLOCK_SIZE * bx;
 
-	//Результирующая матрица
+	
 	C[ic + N * ty + tx] = sum;
 }
 
 
-/**
-* Метод уножения матриц на CPU
-* @params: matrix A, matrix B, result matrix C
-*/
+
 
 void cpu_ikj(float* A, float* B, float* C) {
 	for (int i = 0; i < N; i++) {
@@ -63,10 +57,6 @@ void cpu_ikj(float* A, float* B, float* C) {
 	}
 }
 
-/**
-* Метод вывода матриц
-* @params: matrix C
-*/
 void printMatrix(float* C) {
 	for ( int i = 0; i < N; i++){
 		for ( int j = 0; j < N; j++){
@@ -83,7 +73,6 @@ void printMatrix(float* C) {
 int main() {
 	setlocale(LC_ALL, "RUS");
 
-	//Выделяем память под матрицы на хосте
 
 	float *A = (float*) malloc(N * N *sizeof(float));
 	float *B = (float*) malloc(N * N* sizeof(float));
@@ -91,23 +80,19 @@ int main() {
 	float *C_CPU = (float*) malloc(N * N*  sizeof(float));
 
 
-	// Заполняем матрицы 
+
 	for (int i = 0; i < N; i++)
 		for (int j = 0; j < N; j++) {
 			A[i + j * N] = i + j;
 			B[i + j * N] = i + j;
 		}
-	//printMatrix(A);
-	//printMatrix(B);
-	//Конфигурация запуска ядра
 
-	dim3 dimGrid(N / BLOCK_SIZE, N / BLOCK_SIZE);//Размер используемого грида
-	dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);//Размер используемого блока
+
+	dim3 dimGrid(N / BLOCK_SIZE, N / BLOCK_SIZE);
+	dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
 
 	cudaEvent_t start;
 	cudaEvent_t stop;
-
-	//Создаем event'ы для синхронизации и замера времени работы GPU
 
 	CUDA_CHECK_ERROR(cudaEventCreate(&start));
 	CUDA_CHECK_ERROR(cudaEventCreate(&stop));
@@ -115,39 +100,37 @@ int main() {
 
 	float* adev, *bdev, *cdev;
 
-	//Выделяем глобальную память для храния данных на девайсе
+
 	CUDA_CHECK_ERROR(cudaMalloc((void**)&adev, N * N * sizeof(float *)));
 	CUDA_CHECK_ERROR(cudaMalloc((void**)&bdev, N * N * sizeof(float *)));
 	CUDA_CHECK_ERROR(cudaMalloc((void**)&cdev, N * N * sizeof(float *)));
 
-	//Копируем исходные матрицы с хоста на девайс
+	
 	CUDA_CHECK_ERROR(cudaMemcpy(adev, A, N * N * sizeof(float *), cudaMemcpyHostToDevice));
 	CUDA_CHECK_ERROR(cudaMemcpy(bdev, B, N * N * sizeof(float *), cudaMemcpyHostToDevice));
 
-	//Отмечаем старт расчетов на GPU
+	
 	cudaEventRecord(start, 0);    
 								
 
 	matMult << < dimGrid, dimBlock >> > (adev, bdev , cdev);
 
-	//Отмечаем окончание расчета
 	cudaEventRecord(stop, 0);    
 
 	float timeGPU = 0;
 
-	//Синхронизируемя с моментом окончания расчетов
+	
 	cudaEventSynchronize(stop);   
 
-	//Рассчитываем время работы GPU
+	
 	cudaEventElapsedTime(&timeGPU, start, stop);    
 
 	std::cout << "Время умножения матриц размером " << N << "x" << N << " на GPU = " << timeGPU << " мсек" << std::endl;
 
-	//Копируем результат с девайса на хост
+	
 	CUDA_CHECK_ERROR(cudaMemcpy(C_GPU, cdev, N * N * sizeof(float *), cudaMemcpyDeviceToHost));
 
-	//printMatrix(C_GPU);
-	//Чистим ресурсы на видеокарте
+	
 
 	CUDA_CHECK_ERROR(cudaEventDestroy(start));
 	CUDA_CHECK_ERROR(cudaEventDestroy(stop));
@@ -164,7 +147,7 @@ int main() {
 
 	std::cout << "Время умножения матриц размером " << N << "x" << N << " на CPU = " << ((end_time - start_time)) *1000 / CLOCKS_PER_SEC << " мсек" << std::endl;
 	
-	//Чистим память на хосте
+	
 	delete A;
 	delete B;
 	delete C_GPU;
